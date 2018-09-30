fe2dab00e0570af6c2d6306e88dbf1e7c92b498d
Richard Feldman <richard.t.feldman@gmail.com>
2018-09-30 09:17:20 -0400

    Fix tests.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,4 +1,4 @@
-module Tests exposing (..)
+module Tests exposing (all, expectErr, isError, runWith)
 
 import Expect exposing (Expectation)
 import Json.Decode as Decode exposing (Decoder, null, string)
@@ -8,7 +8,7 @@ import Json.Decode.Pipeline
         , optionalAt
         , required
         , requiredAt
-        , resolveResult
+        , resolve
         )
 import Test exposing (..)
 
@@ -16,8 +16,9 @@ import Test exposing (..)
 {-| Run some JSON through a Decoder and return the result.
 -}
 runWith : String -> Decoder a -> Result String a
-runWith =
-    flip Decode.decodeString
+runWith str decoder =
+    Decode.decodeString decoder str
+        |> Result.mapError Decode.errorToString
 
 
 isError : Result err ok -> Bool
@@ -33,7 +34,7 @@ isError result =
 expectErr : Result err ok -> Expectation
 expectErr result =
     isError result
-        |> Expect.true ("Expected an Err but got " ++ toString result)
+        |> Expect.true ("Expected an Err but got " ++ Debug.toString result)
 
 
 all : Test
@@ -42,72 +43,72 @@ all =
         "Json.Decode.Pipeline"
         [ test "should decode basic example" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> required "a" string
                     |> required "b" string
                     |> runWith """{"a":"foo","b":"bar"}"""
                     |> Expect.equal (Ok ( "foo", "bar" ))
         , test "should decode requiredAt fields" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> requiredAt [ "a" ] string
                     |> requiredAt [ "b", "c" ] string
                     |> runWith """{"a":"foo","b":{"c":"bar"}}"""
                     |> Expect.equal (Ok ( "foo", "bar" ))
         , test "should decode optionalAt fields" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optionalAt [ "a", "b" ] string "--"
                     |> optionalAt [ "x", "y" ] string "--"
                     |> runWith """{"a":{},"x":{"y":"bar"}}"""
                     |> Expect.equal (Ok ( "--", "bar" ))
         , test "optional succeeds if the field is not present" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optional "a" string "--"
                     |> optional "x" string "--"
                     |> runWith """{"x":"five"}"""
                     |> Expect.equal (Ok ( "--", "five" ))
         , test "optional succeeds with fallback if the field is present but null" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optional "a" string "--"
                     |> optional "x" string "--"
                     |> runWith """{"a":null,"x":"five"}"""
                     |> Expect.equal (Ok ( "--", "five" ))
         , test "optional succeeds with result of the given decoder if the field is null and the decoder decodes nulls" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optional "a" (null "null") "--"
                     |> optional "x" string "--"
                     |> runWith """{"a":null,"x":"five"}"""
                     |> Expect.equal (Ok ( "null", "five" ))
         , test "optional fails if the field is present but doesn't decode" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optional "a" string "--"
                     |> optional "x" string "--"
                     |> runWith """{"x":5}"""
                     |> expectErr
         , test "optionalAt fails if the field is present but doesn't decode" <|
             \() ->
-                Decode.succeed (,)
+                Decode.succeed Tuple.pair
                     |> optionalAt [ "a", "b" ] string "--"
                     |> optionalAt [ "x", "y" ] string "--"
                     |> runWith """{"a":{},"x":{"y":5}}"""
                     |> expectErr
-        , test "resolveResult bubbles up decoded Err results" <|
+        , test "resolve bubbles up decoded Err results" <|
             \() ->
-                Decode.succeed Err
+                Decode.succeed Decode.fail
                     |> required "error" string
-                    |> resolveResult
+                    |> resolve
                     |> runWith """{"error":"invalid"}"""
                     |> expectErr
-        , test "resolveResult bubbles up decoded Ok results" <|
+        , test "resolve bubbles up decoded Ok results" <|
             \() ->
-                Decode.succeed Ok
+                Decode.succeed Decode.succeed
                     |> required "ok" string
-                    |> resolveResult
+                    |> resolve
                     |> runWith """{"ok":"valid"}"""
                     |> Expect.equal (Ok "valid")
         ]
